c2e0cc6a9dc741c9f016a707252269f71d286f8f
Malachi McIntosh <mmcintosh@planittesting.com>
2018-04-27 16:45:27 +1200

    Created template step definitions for BuildYourOwnCheapComputer feature. Created pom.xml.

---
+++ src/test/java/stepdefinitions/BuildYourOwnCheapComputer.java
@@ -0,0 +1,53 @@
+package stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java8.En;
+
+public class BuildYourOwnCheapComputer implements En {
+    public BuildYourOwnCheapComputer() {
+        Given("^I'm on the home page$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        When("^I navigate to the cheap pc page$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Add the cheap pc to cart$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Navigate to the shopping cart$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Click agree with terms of service$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Click the checkout button$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Login to checkout$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Select a country$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Input a city$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Fill out address line (\\d+)$", (Integer arg0) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^Fill out zip postal$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}
